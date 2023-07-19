595cbb12e1b112cc929d3b52a0ecafd61f93deea
Yooinhak <syu3236@gmail.com>
2023-07-19 15:35:53 +0900

    component 절대경로 설정

--- pages/components/Input/component.tsx
+++ pages/components/Input/component.tsx
@@ -1,13 +1,14 @@
-import Head from 'next/head';
 import styled from 'styled-components';
-import Input from '@components/Input';
 
-export default function ClientSide() {
+const Component = () => {
   return (
     <Wrapper>
       <Input />
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div``;
+const Input = styled.input``;
+
+export default Component;
